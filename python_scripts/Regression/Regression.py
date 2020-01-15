41a5026fd9935fbbb3d2680cd60faae3f0a734d3
Unknown <buggitheclown@web.de>
2020-01-15 11:37:30 +0100

    Final regression plots + refactoring

--- python_scripts/Regression/Regression.py
+++ python_scripts/Regression/Regression.py
@@ -11,6 +11,7 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.linear_model import ElasticNet
 
+createPlots = False
 plotFolderPath = './Plots/'
 
 # read csv
@@ -63,43 +64,79 @@ plt.close()
 #     'Avg_Contribution_Index_Top','Percentage_Isolated_Actors','Percentage_Closed_Issues']]
 # )
 
-# train test split
-train, test = train_test_split(df, test_size=0.2)
-X_train = train.drop('Target', axis=1)
-y_train = train.loc[:, ['Target']]
-X_test = test.drop('Target', axis=1)
-y_test = test.loc[:, ['Target']]
+if not createPlots:
+    n_runs = 20
+    performance = pd.DataFrame(columns=['Run', 'MSE', 'R2','alpha','l1_ratio'])
 
-# create and train model
-parameters = {
-    'alpha': [0.1,0.25,0.5,0.75,1, 2, 5, 10,25,50,100,200],
-    'l1_ratio': [0,0.1, 0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9, 1],
-    'normalize': [True]
-}
-regr = GridSearchCV(estimator=ElasticNet(), param_grid=parameters, n_jobs=-1, cv=5)
-regr.fit(X_train, y_train)
+    for i in range(n_runs):
+        # train test split
+        train, test = train_test_split(df, test_size=0.1)
+        X_train = train.drop('Target', axis=1)
+        y_train = train.loc[:, ['Target']]
+        X_test = test.drop('Target', axis=1)
+        y_test = test.loc[:, ['Target']]
 
-# predict and evaluate
-y_pred = regr.predict(X_test)
-mse = mean_squared_error(y_test, y_pred)
-r2 = r2_score(y_test, y_pred)
+        # create and train model
+        parameters = {
+            'alpha': [0.1, 0.25, 0.5, 0.75, 1, 2, 5, 10, 25, 50, 100, 200],
+            'l1_ratio': [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1],
+            'normalize': [True]
+        }
+        regr = GridSearchCV(estimator=ElasticNet(), param_grid=parameters, n_jobs=-1, cv=5, scoring='r2')
+        regr.fit(X_train, y_train)
 
-# Plot outputs
-for column in X_train:
-    plot_X = X_test.loc[:, column]
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    ax.scatter(X_train.loc[:, column], y_train, color='black', alpha=0.2)
-    ax.scatter(plot_X, y_test, color='black')
-    ax.scatter(plot_X, y_pred, color='red')
-    ax.set_xlabel(column)
-    ax.set_ylabel('Repository Stars divided by Pull Requests')
-    ax.set_title('Predictions')
-    plt.savefig(fname=plotFolderPath + column + '_scatter.png')
-    plt.close()
+        # predict and evaluate
+        y_pred = regr.predict(X_test)
+        mse = mean_squared_error(y_test, y_pred)
+        r2 = r2_score(y_test, y_pred)
+        performance = performance.append({'Run': i, 'MSE': mse, 'R2': r2, 'alpha': regr.best_estimator_.alpha,
+                                          'l1_ratio': regr.best_estimator_.l1_ratio}, ignore_index=True)
 
-print('Best score:', regr.best_score_)
-print('Evaluation MSE: ' + str(mse))
-print('Evaluation R2: ' + str(r2))
-print('Coefficients: ')
-print(regr.best_estimator_.coef_)
+    performance.to_csv('regression_performance.csv', sep=',', encoding='utf-8', index=False)
+
+if createPlots:
+    # train test split
+    train, test = train_test_split(df, test_size=0.1)
+    X_train = train.drop('Target', axis=1)
+    y_train = train.loc[:, ['Target']]
+    X_test = test.drop('Target', axis=1)
+    y_test = test.loc[:, ['Target']]
+
+    # create and train model
+    parameters = {
+        'alpha': [0.1, 0.25, 0.5, 0.75, 1, 2, 5, 10, 25, 50, 100, 200],
+        'l1_ratio': [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1],
+        'normalize': [True],
+        'selection': ['random','cyclic']
+
+    }
+    regr = GridSearchCV(estimator=ElasticNet(), param_grid=parameters, n_jobs=-1, cv=5, scoring='r2')
+    regr.fit(X_train, y_train)
+
+    # predict and evaluate
+    y_pred = regr.predict(X_test)
+    mse = mean_squared_error(y_test, y_pred)
+    r2 = r2_score(y_test, y_pred)
+
+    # Plot outputs
+    for column in X_train:
+        plot_X = X_test.loc[:, column]
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.scatter(X_train.loc[:, column], y_train, color='black', alpha=0.2)
+        ax.scatter(plot_X, y_test, color='black')
+        ax.scatter(plot_X, y_pred, color='red')
+        ax.set_xlabel(column)
+        ax.set_ylabel('Repository Stars divided by Pull Requests')
+        ax.set_title('Predictions')
+        plt.savefig(fname=plotFolderPath + column + '_scatter.png')
+        plt.close()
+
+
+
+# print('Best score:', regr.best_score_)
+# print('Evaluation MSE: ' + str(mse))
+# print('Evaluation R2: ' + str(r2))
+# print('Coefficients: ')
+# print(regr.best_estimator_.coef_)
+
